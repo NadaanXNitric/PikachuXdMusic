5d943f1bf2f40ab75c4d987cf0334c497fbbbcc0
lukka12 <47463180+lukka12@users.noreply.github.com>
2021-09-17 11:59:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,31 +1,32 @@
-
 import os
 from os import getenv
-
 from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
     load_dotenv("local.env")
 
+load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Sanki_BOTs")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/10a6ff6687de32dac14a3.png")
+BOT_NAME = getenv("BOT_NAME", "Pɪᴋᴀᴄʜᴜ • Mᴜsɪᴄ")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/20147c4f049e2c1f2f248.png")
+THUMB_IMG = getenv("THUMB_IMG", None)
+AUD_IMG = getenv("AUD_IMG", None)
+QUE_IMG = getenv("QUE_IMG", None)
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
+BOT_USERNAME = getenv("BOT_USERNAME", "PikachuXdBot")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "PikachuXdAssistant")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "Sanki_BotsSupport")
-PROJECT_NAME = getenv("PROJECT_NAME", "PikachuXMusic")
-SOURCE_CODE = getenv("SOURCE_CODE", "https://t.me/iTs_Nitric")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
-ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Sanki_BOTs_Support")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Sanki_BOTs")
+OWNER_NAME = getenv("OWNER_NAME", "iTs_Nitric") # isi dengan username kamu tanpa simbol @
+DEV_NAME = getenv("DEV_NAME", "iTs_Nitric")
 PMPERMIT = getenv("PMPERMIT", None)
-LOG_GRP = getenv("LOG_GRP", "1001522296749")
-OWNER_NAME = getenv("OWNER_NAME", "iTs_Nitric")
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
