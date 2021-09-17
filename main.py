24c80b6163a94fa50baccf260edd95d98e059f39
lukka12 <47463180+lukka12@users.noreply.github.com>
2021-09-17 11:46:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import Client as Bot
 
 from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from services.callsmusic import run
+from PikachuXd.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
