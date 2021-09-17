53eb39d42782a20387ea85ad54d7a404e0782b5c
lukka12 <47463180+lukka12@users.noreply.github.com>
2021-09-17 11:15:20 +0530

    Add files via upload

---
+++ PikachuXd/__main__.py
@@ -0,0 +1,23 @@
+import requests
+from pyrogram import Client as Bot
+
+from callsmusic import run
+from PikachuXd.config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
+
+response = requests.get(BG_IMAGE)
+with open("./etc/foreground.png", "wb") as file:
+    file.write(response.content)
+
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="PikachuXd/handlers"),
+)
+
+print("[INFO]:  STARTED Join @Sanki_BOTs!")
+
+bot.start()
+run()
